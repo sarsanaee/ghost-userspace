3039f562ffedc31a5c200dbab9027e794fbba635
Alireza Sanaee <sarsanaee@gmail.com>
2022-06-21 14:20:57 +0000

    Renaming FIFO -> Fifo for function names, and revised the throughput range

--- experiments/scripts/fifo_queuing.py
+++ experiments/scripts/fifo_queuing.py
@@ -12,14 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Runs the RocksDB FIFO centralized-queuing experiments.
+Runs the RocksDB Fifo centralized and per core queuing experiments.
 
-This script runs the FIFO centralized and FIFO per core queueing with RocksDB
+This script runs the Fifo centralized and Fifo per core queueing with RocksDB
 ghOSt. In these experiments, there is a centralized queue maintained for
-RocksDB requests and the requests are not reordered or preempted. This script
-should be run on a machine with an Intel Xeon Platinum 8173M as that is what we
-used in the paper. If another CPU is used, the throughput ranges below should
-be adjusted.  
+RocksDB requests and the requests are not reordered or preempted. 
 """
 
 from typing import Sequence
@@ -38,16 +35,16 @@ _NUM_CFS_WORKERS = _NUM_CPUS - 2
 _NUM_GHOST_WORKERS = 18 
 
 
-def RunGhostFIFOPerCore():
+def RunGhostFifoPerCore():
   """Runs the ghOSt experiment."""
   e: Experiment = Experiment()
   # Run throughputs 10000, 20000, 30000, ..., 420000.
-  # e.throughputs = list(i for i in range(10000, 421000, 10000))
+  e.throughputs = list(i for i in range(10000, 421000, 10000))
   # Toward the end, run throughputs 430000, 431000, 432000, ..., 460000.
-  # e.throughputs.extend(list(i for i in range(430000, 461000, 1000)))
-  e.throughputs = [700000]
+  e.throughputs.extend(list(i for i in range(430000, 461000, 1000)))
   e.rocksdb = GetRocksDBOptions(Scheduler.GHOST, _NUM_CPUS, _NUM_GHOST_WORKERS)
   e.rocksdb.get_exponential_mean = '1us'
+  # Fifo per core scheduler does not need PRIO_TABLE and relies on FUTEX.
   e.rocksdb.ghost_wait_type = GhostWaitType.FUTEX
   e.antagonist = None
   e.ghost = GetGhostOptions(_NUM_CPUS)
@@ -55,16 +52,16 @@ def RunGhostFIFOPerCore():
 
   Run(e)
 
-def RunGhostFIFOCentralized():
+def RunGhostFifoCentralized():
   """Runs the ghOSt experiment."""
   e: Experiment = Experiment()
   # Run throughputs 10000, 20000, 30000, ..., 420000.
-  # e.throughputs = list(i for i in range(10000, 421000, 10000))
+  e.throughputs = list(i for i in range(10000, 421000, 10000))
   # Toward the end, run throughputs 430000, 431000, 432000, ..., 460000.
-  # e.throughputs.extend(list(i for i in range(430000, 461000, 1000)))
-  e.throughputs = [700000]
+  e.throughputs.extend(list(i for i in range(430000, 461000, 1000)))
   e.rocksdb = GetRocksDBOptions(Scheduler.GHOST, _NUM_CPUS, _NUM_GHOST_WORKERS)
   e.rocksdb.get_exponential_mean = '1us'
+  # Fifo centralized scheduler does not need PRIO_TABLE and relies on FUTEX.
   e.rocksdb.ghost_wait_type = GhostWaitType.FUTEX
   e.antagonist = None
   e.ghost = GetGhostOptions(_NUM_CPUS)
@@ -76,6 +73,9 @@ def main(argv: Sequence[str]):
   if len(argv) > 4:
     raise app.UsageError('Too many command-line arguments.')
   elif len(argv) == 2:
+    # We do not need ghost parameter in this particular code, because we just
+    # have fifo. However, I just put ghost parameter in case we wanted to add
+    # CFS in this script as well.
     raise app.UsageError(
         'No experiment specified. Pass `ghost` for the scheduler and\
         `fifo-centralized` and/or `fifo-per-core` for the ghost policy as\
@@ -92,13 +92,12 @@ def main(argv: Sequence[str]):
     if scheduler != Scheduler.GHOST:
       raise ValueError(f'Unknown scheduler {scheduler}.')
 
-    # It is a ghost scheduler
-    # check the policies.
+    # It is a ghost scheduler, let's check the policies.
     if policy == Policy.FIFO_CENTRALIZED:
-      RunGhostFIFOCentralized()
+      RunGhostFifoCentralized()
     # check the policies.
     elif policy == Policy.FIFO_PER_CORE:
-      RunGhostFIFOPerCore()
+      RunGhostFifoPerCore()
     else:
       raise ValueError(f'Unknown policy {policy}.')
 
